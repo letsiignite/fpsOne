54ba364f2e63ab88e37515c03a5e0a80b930ad7a
letsiignite@gmail.com <nagarajrjoshi@gmail.com>
2025-08-30 15:14:37 +0530

    Updates to AI and player script

---
+++ Assets/Scripts/Game/Bullet.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace Game
+{
+
+
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField]
+        private float damage = 10;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public float GetDamage()
+        {  return damage; }
+    }
+}
